2dab3da529579aa620a4646b13323ea54c6e8449
zaiaaa <gustavozaia75@gmail.com>
2025-08-24 15:03:39 -0300

    style Alterações de cores e cursor pointer quando mouse sobre o botao

--- script.js
+++ script.js
@@ -4,12 +4,11 @@ document.getElementById("Botao").onclick = () => {
     contador++
     let toast = document.getElementById("toast")
     if(!toast){
-        toast = document.createElement("div")
-        toast.id = "toast"
-        toast.className = "toast"
-        document.body.appendChild(toast)
+        toast = document.createElement("div");
+        toast.id = "toast";
+        toast.className = "toast";
+        document.body.appendChild(toast);
     }
-
-    toast.textContent = `Você clicou ${contador}`
+    toast.innerHTML = `Você clicou <span class="span">${contador}</span> vezes`
     toast.classList.add("show")
 }
